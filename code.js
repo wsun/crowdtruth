0e1e7aecb61734dea6834c2f63ae0efdc0bcaf26
Will Sun <wjy.sun@gmail.com>
2012-10-10 02:32:33 -0400

    up to source process

--- code.js
+++ code.js
@@ -1,36 +1,48 @@
-
-
 // improve text
 var speech = "First, we've got to improve our education system. And we've made enormous progress drawing on ideas both from Democrats and Republicans that are already starting to show gains in some of the toughest-to- deal-with schools. We've got a program called Race to the Top that has prompted reforms in 46 states around the country, raising standards, improving how we train teachers. So now I want to hire another hundred thousand new math and science teachers and create 2 million more slots in our community colleges so that people can get trained for the jobs that are out there right now. And I want to make sure that we keep tuition low for our young people."
+var author = "Barack Obama"
 
-var hitId = createIdentifyHIT(speech, 0.01)
-var hit = mturk.waitForHIT(hitId)
+var hitId0 = createIdentifyHIT(speech, 0.01)
+var hit0 = mturk.waitForHIT(hitId0)
 
-var newText1 = hit.assignments[0].answer.identifyFacts1
-var newText2 = hit.assignments[0].answer.identifyFacts2
-var newText3 = hit.assignments[0].answer.identifyFacts3
-var newText4 = hit.assignments[0].answer.identifyFacts4
-var newText5 = hit.assignments[0].answer.identifyFacts5
+// collect results
+var facts = new Array()
+foreach(hit0.assignments[0].answer, function(answer) {
+    if (answer) {
+        facts.push(answer)
+    }
+})
 
-print("-------------------")
-print(newText1)
-print(newText2)
-print(newText3)
-print(newText4)
-print(newText5)
-print("-------------------")
+// check facts
+var hitId1 = createCheckHIT(facts, 0.02)
+var hit1 = mturk.waitForHIT(hitId1)
+var checkedFacts = checkFacts(facts, hit1)
+mturk.approveAssignments(hit1.assignments)
 
-/*
-// verify improvement
-if (vote(text, newText, 0.01)) {
-    text = newText
-    mturk.approveAssignment(hit.assignments[0])
-    print("\nvote = keep\n")
-} else {
-    mturk.rejectAssignment(hit.assignments[0])
-    print("\nvote = reject\n")
+// approve if less than one error
+if (facts.length - checkedFacts.length < 2) {
+    mturk.approveAssignment(hit0.assignments[0])
 }
-*/    
+else {
+    mturk.rejectAssignment(hit0.assignments[0])
+}
+
+// find sources
+var sources = new Array()
+foreach(facts, function(fact) {
+    var hitId2 = createSourceHIT(fact, author, 0.05)
+    var hit2 = mturk.waitForHIT(hitId2)
+    sources.push(hit2)
+})
+
+// process sources
+// process(sources)
+
+
+
+print("------------------")
+print(sources)
+print("------------------")
 
 
 
@@ -42,7 +54,7 @@ function createIdentifyHIT(speechText, identifyCost) {
             <IsRequired>true</IsRequired>
             <QuestionContent>
                 <FormattedContent><![CDATA[
-                    <p><strong>Please identify up to 5 factual claims in the below excerpt. These will be reviewed for their accuracy. 
+                    <p><strong>Please identify up 3-5 factual claims in the below excerpt. These will be reviewed for their accuracy. 
                     Copy-paste these claims into each of the text boxes below.</strong></p>
                 ]]></FormattedContent>
                 <Text>{speechText}</Text>
@@ -122,8 +134,222 @@ function createIdentifyHIT(speechText, identifyCost) {
                             maxAssignments: 1})
 }
 
-function createCheckHIT(factText, checkCost) {
+function createCheckHIT(facts, checkCost) {
+    default xml namespace = "http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2005-10-01/QuestionForm.xsd";
+    var q = <QuestionForm></QuestionForm>
+    var num = 0
+    foreach(facts, function(fact) {
+        var id = "voteFacts" + num
 
+        default xml namespace = "http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2005-10-01/QuestionForm.xsd";
+        q.Question += 
+            <Question>
+                <QuestionIdentifier>{id}</QuestionIdentifier>
+                <IsRequired>true</IsRequired>
+                <QuestionContent>
+                    <FormattedContent><![CDATA[
+                        <p><strong>Is this a coherent, factual claim?</strong></p>
+                    ]]></FormattedContent>
+                    <Text>{fact}</Text>
+                </QuestionContent>
+                <AnswerSpecification>
+                    <SelectionAnswer>
+                        <Selections>
+                            <Selection>
+                                <SelectionIdentifier>yes</SelectionIdentifier>
+                                <Text>Yes</Text>
+                            </Selection>
+                            <Selection>
+                                <SelectionIdentifier>no</SelectionIdentifier>
+                                <Text>No</Text>
+                            </Selection>
+                        </Selections>
+                    </SelectionAnswer>
+                </AnswerSpecification>
+            </Question>
+        num += 1
+    })
+    return mturk.createHIT({title: "Vote on Factual Claims",
+                            desc: "Decide if a political statement is a factual claim",
+                            question: "" + q,
+                            reward: checkCost,
+                            assignmentDurationInSeconds: 5 * 60, 
+                            maxAssignments: 2 })
+}
+
+// helper function to count votes 
+function checkFacts(facts, hit) {
+    var count = 0
+    var sums = new Array()
+    var result = new Array()
+
+    for (var i = 0; i < hit.assignments.length; i++) {
+        if (i == 0) {
+            foreach(hit.assignments[i].answer, function(answer) {
+                if (answer == 'yes') {
+                    sums.push(1)
+                }
+                else {
+                    sums.push(0)
+                }
+                count += 1
+            })
+        }
+        else {
+            foreach(hit.assignments[i].answer, function(answer) {
+                if (answer == 'yes') {
+                    sums[count] += 1
+                }
+                count += 1
+            })
+        }
+        count = 0
+    }
+
+    for (var i = 0; i < sums.length; i++) {
+        if (sums[i] > 0) {
+            print("IN")
+            result.push(facts[i])
+        }
+    }
+
+    return result
+}
+
+function createSourceHIT(fact, author, sourceCost) {
+    default xml namespace = "http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2005-10-01/QuestionForm.xsd";
+    var q = <QuestionForm>
+        <Question>
+            <QuestionIdentifier>sourceLink</QuestionIdentifier>
+            <IsRequired>true</IsRequired>
+            <QuestionContent>
+                <FormattedContent><![CDATA[
+                    <p><strong>Please find a reputable third-party source that supports or refutes this factual claim.</strong></p>
+                ]]></FormattedContent>
+                <Text>{author}: {fact}</Text>
+                <Text>Paste the link to your source.</Text>
+            </QuestionContent>
+            <AnswerSpecification>
+                <FreeTextAnswer>
+                    <Constraints>
+                        <Length minLength="2" maxLength="500"></Length>
+                        <AnswerFormatRegex regex="\S" errorText="The content cannot be blank."/>
+                    </Constraints>
+                    <NumberOfLinesSuggestion>1</NumberOfLinesSuggestion>
+                </FreeTextAnswer>
+            </AnswerSpecification>
+        </Question>
+        <Question>
+            <QuestionIdentifier>sourceNote</QuestionIdentifier>
+            <IsRequired>true</IsRequired>
+            <QuestionContent>
+                <Text>Paste the relevant passage or describe the evidence provided.</Text>
+            </QuestionContent>
+            <AnswerSpecification>
+                <FreeTextAnswer>
+                    <Constraints>
+                        <Length minLength="2" maxLength="500"></Length>
+                        <AnswerFormatRegex regex="\S" errorText="The content cannot be blank."/>
+                    </Constraints>
+                    <NumberOfLinesSuggestion>4</NumberOfLinesSuggestion>
+                </FreeTextAnswer>
+            </AnswerSpecification>
+        </Question>
+        <Question>
+            <QuestionIdentifier>sourceValidity</QuestionIdentifier>
+            <IsRequired>true</IsRequired>
+            <QuestionContent>
+                <Text>Does your evidence support or refute the claim?</Text>
+            </QuestionContent>
+            <AnswerSpecification>
+                <SelectionAnswer>
+                    <Selections>
+                        <Selection>
+                            <SelectionIdentifier>support</SelectionIdentifier>
+                            <Text>Support</Text>
+                        </Selection>
+                        <Selection>
+                            <SelectionIdentifier>refute</SelectionIdentifier>
+                            <Text>Refute</Text>
+                        </Selection>
+                    </Selections>
+                </SelectionAnswer>
+            </AnswerSpecification>
+        </Question>
+    </QuestionForm>
+
+    return mturk.createHIT({title : "Investigate a Factual Claim", 
+                            desc : "Investigate whether a political claim is valid",
+                            question : "" + q, 
+                            reward : sourceCost, 
+                            assignmentDurationInSeconds : 10 * 60, 
+                            maxAssignments: 2})
+}
+
+// helper function to extract source info
+function process(sources) {
+    var section = new Array()
+
+    foreach(sources, function(hit) {
+        var links = new Array()
+        var notes = new Array()
+        var labels = new Array()
+
+        foreach(hit.assignment, function(assign){
+            links.push(assign.answer.sourceLink)
+            notes.push(assign.answer.sourceNote)
+            labels.push(assign.answer.sourceValidity)
+        })
+
+        section.push({ fact: links: link, notes: note, labels: label })
+    })
+
+
+    return section
+}
+
+// TODO 
+function createVoteHIT(sources, voteCost) {
+    default xml namespace = "http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2005-10-01/QuestionForm.xsd";
+    var q = <QuestionForm></QuestionForm>
+    var num = 0
+    foreach(sources, function(source) {
+        var id = "voteSource" + num
+        print(id)
+
+        default xml namespace = "http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2005-10-01/QuestionForm.xsd";
+        q += 
+            <Question>
+                <QuestionIdentifier>{id}</QuestionIdentifier>
+                <IsRequired>true</IsRequired>
+                <QuestionContent>
+                    <FormattedContent><![CDATA[
+                        <p><strong>Is this a factual claim?</strong></p>
+                    ]]></FormattedContent>
+                    <Text>{fact}</Text>
+                </QuestionContent>
+                <AnswerSpecification>
+                    <SelectionAnswer>
+                        <Selections>
+                            <Selection>
+                                <SelectionIdentifier>yes</SelectionIdentifier>
+                                <Text>Yes</Text>
+                            </Selection>
+                            <Selection>
+                                <SelectionIdentifier>no</SelectionIdentifier>
+                                <Text>No</Text>
+                            </Selection>
+                        </Selections>
+                    </SelectionAnswer>
+                </AnswerSpecification>
+            </Question>
+    })
+    return mturk.createHIT({title: "Vote on Factual Claims",
+                            desc: "Decide if a political statement is a factual claim",
+                            question: "" + q,
+                            reward: checkCost,
+                            assignmentDurationInSeconds: 5 * 60, 
+                            maxAssignments: 3 })
 }
 
 function vote(textA, textB, voteCost) {
